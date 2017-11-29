75eb2ba55c5f9caa0e6b19571fdbca9ebe8f74c5
shir19-meet <shir19@meet.mit.edu>
2017-11-29 18:56:06 +0200

    lab5

--- Lab5/lab 5.py
+++ Lab5/lab 5.py
@@ -1,11 +1,11 @@
 from turtle import Turtle
 import turtle
 
-##class Square(Turtle):
-##    def __init__(self,size, shape,):
-##        Turtle.__init__(self)
-##        self.shape("square")
-##        self.shapesize(size)
+class Square(Turtle):
+    def __init__(self,size, shape,):
+        Turtle.__init__(self)
+        self.shape("square")
+        self.shapesize(size)
        
 
 ##    def random_color(self):
@@ -16,14 +16,54 @@ import turtle
         
 ##
 ##s1 = Square("square",10,)
-turtle.register_shape("hexagon",((0,0),(50,0),(75,-25),(50,-50),(0,-50),(-25,-25),(0,0)))
-
 class Hexagon(Turtle):
-    def __init__ (self,size):
-        turtle.__init__(self)
-        self.shapesize(size)
+    def __init__(self,size,speed,color):
+        Turtle.__init__(self)
+        self.speed(speed)
+        self.color(color)
+        
+        self.home()
+        self.penup()
+        self.begin_poly()
+        self.fd(100)
+        self.left(-300)
+        self.fd(100)
+        self.right(300)
+        self.fd(100)
+        self.right(300)
+        self.fd(100)
+        self.left(-300)
+        self.fd(100)
+        self.left(-300)
+        self.fd(100)
+        self.end_poly()
+        p = self.get_poly()
+        turtle.register_shape("hexagon",p)
         self.shape("hexagon")
 
+    def draw_triangle(self,size):
+        self.pendown()
+        self.forward(size)
+        self.right(120)
+        self.forward(size)
+        self.right(120)
+        self.forward(size)
 
-h1= (10)
     
+    
+        
+
+h1=Hexagon(10,0,"blue")
+h1.draw_triangle(100)
+
+
+##
+##
+##    def __init__ (self,size):
+##        turtle.__init__(self)
+##        self.shapesize(size)
+##        self.shape("hexagon")
+##
+##
+#h1= (10)
+
